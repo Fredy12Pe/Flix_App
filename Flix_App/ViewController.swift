471910e47f45741deb51027ed902f7ca48d916f1
Fredy <fapedro@uci.edu>
2020-01-19 18:42:31 -0800

    Initial Commit

---
+++ Flix_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix_App
+//
+//  Created by Fredy Pedro on 1/19/20.
+//  Copyright © 2020 Fredy Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
