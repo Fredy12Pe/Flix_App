9e436c641bd7947844710d602b1a95c3b7e0978d
Fredy <fapedro@uci.edu>
2020-01-22 12:25:07 -0800

    Added Image View, Tittle, and description

---
+++ Flix_App/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix_App
+//
+//  Created by Fredy Pedro on 1/22/20.
+//  Copyright © 2020 Fredy Pedro. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
